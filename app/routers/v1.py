262252a1251c18ce8af3a436aa6643a07a7aae26
KevinSuh6433 <swt8069kevin@gmail.com>
2021-08-15 16:35:38 -0400

    added abstraction interface for different views

--- app/routers/v1.py
+++ app/routers/v1.py
@@ -2,50 +2,77 @@
 from fastapi import APIRouter
 
 from ..services.location.jhu import get_category
+from ..services.location import LocationService
 
 V1 = APIRouter()
 
 
-@V1.get("/all")
-async def all_categories():
-    """Get all the categories."""
-    confirmed = await get_category("confirmed")
-    deaths = await get_category("deaths")
-    recovered = await get_category("recovered")
-
-    return {
-        # Data.
-        "confirmed": confirmed,
-        "deaths": deaths,
-        "recovered": recovered,
-        # Latest.
-        "latest": {
-            "confirmed": confirmed["latest"],
-            "deaths": deaths["latest"],
-            "recovered": recovered["latest"],
-        },
-    }
+class OutputsAbstract():
+    def showOutput(self):
+        raise NotImplemented()
 
 
-@V1.get("/confirmed")
-async def get_confirmed():
-    """Confirmed cases."""
-    confirmed_data = await get_category("confirmed")
-
-    return confirmed_data
+class Bridge(OutputsAbstract):
+    def __init__(self) -> None:
+        self.implemntation = None
 
 
-@V1.get("/deaths")
-async def get_deaths():
-    """Total deaths."""
-    deaths_data = await get_category("deaths")
+class OutputAll(Bridge):
+    def __init__(self, implementation: LocationService):
+        self.implemntation = implementation
 
-    return deaths_data
+    @V1.get("/all")
+    async def showOutput():
+        """Get all the categories."""
+        confirmed = await get_category("confirmed")
+        deaths = await get_category("deaths")
+        recovered = await get_category("recovered")
+
+        return {
+            # Data.
+            "confirmed": confirmed,
+            "deaths": deaths,
+            "recovered": recovered,
+            # Latest.
+            "latest": {
+                "confirmed": confirmed["latest"],
+                "deaths": deaths["latest"],
+                "recovered": recovered["latest"],
+            },
+        }
 
 
-@V1.get("/recovered")
-async def get_recovered():
-    """Recovered cases."""
-    recovered_data = await get_category("recovered")
+class OutputConfirmed(Bridge):
+    def __init__(self, implementation: LocationService):
+        self.implemntation = implementation
 
-    return recovered_data
+    @V1.get("/confirmed")
+    async def showOutput():
+        """Confirmed cases."""
+        confirmed_data = await get_category("confirmed")
+
+        return confirmed_data
+
+
+class OutputDeaths(Bridge):
+    def __init__(self, implementation: LocationService):
+        self.implemntation = implementation
+
+    @V1.get("/deaths")
+    async def showOutput():
+        """Total deaths."""
+        deaths_data = await get_category("deaths")
+
+        return deaths_data
+
+
+class OutputRecovered(Bridge):
+    def __init__(self, implementation: LocationService):
+        self.implemntation = implementation
+
+    @V1.get("/recovered")
+    async def showOutput():
+        """Recovered cases."""
+        recovered_data = await get_category("recovered")
+
+        return recovered_data
